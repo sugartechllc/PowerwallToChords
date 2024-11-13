9f58ea36b4b1ed53167f9c91505e85651f4dc420
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-11-13 11:42:42 -0700

    add averaging and other bells and whistles

--- powerwallToChords.py
+++ powerwallToChords.py
@@ -1,15 +1,84 @@
 import pypowerwall
 import json
 import time
+import zoneinfo
 import logging
 import argparse
 import sys
+import os
+import statistics
+import datetime
 import pychords.tochords as tochords
 
-#pypowerwall.set_debug(True)
-host = password = ""
-email='tickranch@gmail.com'
-timezone = "America/Denver"
+
+class Average:
+    def __init__(self, time, value):
+        self.time = time
+        self.average = value
+class Aggregater: 
+    '''Collect and average a queue of values
+    
+    When an average is called for, the average over all values 
+    is calculated, and then all values except for the last one in the
+    queue is removed.
+    '''
+    def __init__(self, name:str)->None:
+        self.name = name
+        self.times = []
+        self.values = []
+
+    def add(self, value:float, time:str)->None:
+        self.values.append(value)
+        self.times.append(time)
+
+    def avg(self)->list:
+        avg = statistics.mean(self.values)
+        time = statistics.mean(self.times)
+        while len(self.times) > 1:
+            self.values.pop()
+            self.times.pop()
+        return Average(time=time, value=avg)
+
+class PW_Aggregator:
+    def __init__(self, pw:pypowerwall.Powerwall):
+        self.pw = pw
+        self.time_aggregator = Aggregater('time')
+        self.grid_aggregator = Aggregater('grid')
+        self.solar_aggregator = Aggregater('solar')
+        self.battery_aggregator = Aggregater('battery')
+        self.load_aggregator = Aggregater('load')
+        self.level_aggregator = Aggregater('level')
+
+    def poll_pw(self):
+        time = datetime.datetime.fromisoformat(self.pw.grid(verbose=True)['last_communication_time']).timestamp()
+        self.time_aggregator.add(time=time, value=time)
+        power = self.pw.power()
+        self.grid_aggregator.add(time=time, value=power['site'])
+        self.solar_aggregator.add(time=time, value=power['solar'])
+        self.battery_aggregator.add(time=time, value=power['battery'])
+        self.load_aggregator.add(time=time, value=power['load'])
+        self.level_aggregator.add(time=time, value=self.pw.level())
+
+    def avg(self):
+        '''Return a dictionary of Average'''
+        ret_val = {}
+        ret_val['time'] = self.time_aggregator.avg().average
+        ret_val['grid'] = self.grid_aggregator.avg().average
+        ret_val['solar'] = self.solar_aggregator.avg().average
+        ret_val['battery'] = self.battery_aggregator.avg().average
+        ret_val['load'] = self.load_aggregator.avg().average
+        ret_val['level'] = self.level_aggregator.avg().average
+        return ret_val
+
+def check_auth_files(pw_auth_path:str)->bool:
+    # If the authorization files can't be found, return false
+    ok = True
+    auth_files = [f'{pw_auth_path}/.pypowerwall.auth', f'{pw_auth_path}/.pypowerwall.site']
+    for f in auth_files:
+        if not (os.path.isfile(f) and os.access(f, os.R_OK)):
+            print(f'Unable to access tesla credentials file {f}')
+            ok = False
+    return ok
 
 def main(config_file:dict)->None:
 
@@ -17,42 +86,69 @@ def main(config_file:dict)->None:
     tochords.startSender()
 
     # Load configuration
-    logging.info(f"Starting Powerwall to Chords with {config_file}")
+    logging.info(f"Starting powerwallToChords with {config_file}")
     config = json.loads(open(config_file).read())
+
+    # Setting host and password to emty strings causes pypowerwall.Powerwall
+    # to use the cloud api.
     host = ''
     password = ''
-    email = config['tesla']['owner_email']
-    timezone = config['tesla']['timezone']
+    poll_secs = config['powerwalltochords']['poll_secs']
+    avg_count = config['powerwalltochords']['avg_count']
+    pw_auth_path = os.path.expanduser(config['powerwalltochords']['pw_auth_path'])
+    email = config['powerwalltochords']['owner_email']
+    timezone = config['powerwalltochords']['timezone']
+    debug = config['powerwalltochords']['debug']
+
+    if not check_auth_files(pw_auth_path=pw_auth_path):
+        sys.exit(1)
 
     # Connect to Tesla Cloud Powerwall - auto_select mode (local, fleetapi, cloud)
-    pw = pypowerwall.Powerwall(host, password, email, timezone, auto_select=True)
+    pypowerwall.set_debug(debug)
+    pw = pypowerwall.Powerwall(
+        host=host, 
+        password=password, 
+        email=email, 
+        timezone=timezone, 
+        authpath=pw_auth_path, 
+        auto_select=True)
 
     # Some System Info
-    print("Site Name: %s - Firmware: %s - DIN: %s" % (pw.site_name(), pw.version(), pw.din()))
-    print("System Uptime: %s\n" % pw.uptime())
+    print(f'Site Name:{pw.site_name()} Firmware:{pw.version()} DIN:{pw.din()}')
+    print(f'Polling: {poll_secs}s Avg count: {avg_count}')
 
+    count = 0
+    pw_aggregator = PW_Aggregator(pw)
     while True:
-        vars = {}
-        vars['at'] = pw.grid(verbose=True)['last_communication_time']
-        power = pw.power()
-        vars['grid'] = power['site']
-        vars['solar'] = power['solar']
-        vars['battery'] = power['battery']
-        vars['load'] = power['load']
-        vars['level'] = pw.level()
+        pw_aggregator.poll_pw()
+        count += 1
 
-        chords_record = {}
-        chords_record["inst_id"] = config['chords']["instrument_id"]
-        chords_record["api_email"] = config['chords']["api_email"]
-        chords_record["api_key"] = config['chords']["api_key"]
-        chords_record["vars"] = vars
-        uri = tochords.buildURI(config['chords']["chords_host"], chords_record)
-        logging.info(f"Submitting: {uri}")
-        max_queue_length = 31*60*24
-        tochords.submitURI(uri, max_queue_length)
+        if count == avg_count:
+            averaged_values = pw_aggregator.avg()
+            t = averaged_values['time']
+            at = str(datetime.datetime.fromtimestamp(t, zoneinfo.ZoneInfo(timezone)))
+            vars = {}
+            vars['at'] = at
+            vars['grid'] = averaged_values['grid']
+            vars['solar'] = averaged_values['solar']
+            vars['battery'] = averaged_values['battery']
+            vars['load'] = averaged_values['load']
+            vars['level'] = averaged_values['level']
+
+            chords_record = {}
+            chords_record["inst_id"] = config['chords']["instrument_id"]
+            chords_record["api_email"] = config['chords']["api_email"]
+            chords_record["api_key"] = config['chords']["api_key"]
+            chords_record["vars"] = vars
+            uri = tochords.buildURI(config['chords']["chords_host"], chords_record)
+            logging.info(f"Submitting: {uri}")
+            max_queue_length = 31*60*24
+            tochords.submitURI(uri, max_queue_length)
+
+            count = 0
 
         #print({**{'time':pw.grid(verbose=True)['last_communication_time']}, **pw.power(), **{'level':pw.level()}})
-        time.sleep(60)
+        time.sleep(poll_secs)
 
 if __name__ == '__main__':
 
